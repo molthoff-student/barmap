0798a668a9153c4a234044643161a9d9e351c195
molthoff-student <97132891@st.deltion.nl>
2026-08-27 09:34:08 +0200

    Add globally accessible static values for styling

---
+++ src/static.tsx
@@ -0,0 +1,7 @@
+export const ACCENT_COLOR = '#000000';
+export const DEFAULT_COLOR = '#FFFFFF'
+export const OVERLAY_COLOR = 'rgba(207, 207, 207, 0.66)';
+export const HIGHLIGHT_COLOR = '#91C5F2';
+export const LOWLIGHT_COLOR = '#D3D3D3';
+export const DEFAULT_BORDER_WIDTH = 2;
+export const SECTION_BORDER_WIDTH = 3;
