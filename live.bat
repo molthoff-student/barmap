857387d2c59a2a39ed47b29a3621fdc82b2f7c94
molthoff-student <97132891@st.deltion.nl>
2026-09-03 08:03:15 +0200

    Add live batch file

---
+++ live.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting...
+cd /d "C:\Users\mick_\Lokale bestanden\School\2026\VSCode\websites\BarMap" 
+call npx expo run:android --variant release
+echo.
+echo Exit code: %ERRORLEVEL%
+echo.
+cmd /k
+pause
