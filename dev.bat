0b6702adaea2ad8895e38d5a1cef1452c1814ad9
molthoff-student <97132891@st.deltion.nl>
2026-09-03 08:03:00 +0200

    Add dev batch file

--- dev.bat
+++ dev.bat
@@ -1,2 +1,9 @@
+@echo off
+echo Starting...
 cd /d "C:\Users\mick_\Lokale bestanden\School\2026\VSCode\websites\BarMap" 
-npx expo start
+call npx expo start
+echo.
+echo Exit code: %ERRORLEVEL%
+echo.
+cmd /k
+pause
